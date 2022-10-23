c10cbc6e719b9f2aee1cb5bb4c888d974185d2dd
Miguel Reyes <migueldeje171717@gmai.com>
2022-10-22 21:50:22 -0600

    style: edicion de texto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -239,7 +239,7 @@
         <div class="col-lg-5">
           <div class="section-heading">
             <h2>OLPE</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+            <p>Mejora tus habilidades y adquiere nuevos conocimientos y cursos de capacitacion que te ayuden en tu desarrollo profecional.</p>
           </div>
         </div>
       </div>
@@ -351,7 +351,7 @@
             <div class="row">
               <div class="col-lg-12">
                 <div id="map">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12469.776493332698!2d-80.14036379941481!3d25.907788681148624!2m3!1f357.26927939317244!2f20.870722720054623!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x88d9add4b4ac788f%3A0xe77469d09480fcdb!2sSunny%20Isles%20Beach!5e1!3m2!1sen!2sth!4v1642869952544!5m2!1sen!2sth" width="100%" height="550px" frameborder="0" style="border:0; border-radius: 23px; " allowfullscreen=""></iframe>
+                  <iframe src= "" allowfullscreen=""></iframe>
                 </div>
               </div>
             </div>
