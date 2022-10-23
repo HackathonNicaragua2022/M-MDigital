8303f3a788a5b86efb71c7005eaeb5dc471378b1
Miguel Reyes <migueldeje171717@gmai.com>
2022-10-22 18:40:49 -0600

    Feat: imagenes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,6 +60,169 @@
 
                     <!-- *****  Inicio del banners  ***** -->
 
+                    <section id="section-1">
+                        <div class="content-slider">
+                          <input type="radio" id="banner1" class="sec-1-input" name="banner" checked>
+                          <input type="radio" id="banner2" class="sec-1-input" name="banner">
+                          <input type="radio" id="banner3" class="sec-1-input" name="banner">
+                          <input type="radio" id="banner4" class="sec-1-input" name="banner">
+                          <div class="slider">
+                            <div id="top-banner-1" class="banner">
+                              <div class="banner-inner-wrapper header-text">
+                                <div class="main-caption">
+                                  <h2>Siempre hay algo nuevo que puedes hacer</h2>
+                                  <h1>Y tener éxito en ello</h1>
+                                  <div class="border-button"><a href="about.html"><i class="fa fa-user"></i> Mi Perfil</a></div>
+                                </div>
+                                <div class="container">
+                                  <div class="row">
+                                    <div class="col-lg-12">
+                                      <div class="more-info">
+                                        <div class="row">
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-user"></i>
+                                            <h4><span>Population:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-globe"></i>
+                                            <h4><span>Territory:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-home"></i>
+                                            <h4><span>AVG Price:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <div class="main-button">
+                                              <a href="about.html">Explore More</a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div id="top-banner-2" class="banner">
+                              <div class="banner-inner-wrapper header-text">
+                                <div class="main-caption">
+                                  <h2>Take a Glimpse Into The Beautiful Country Of:</h2>
+                                  <h1>Switzerland</h1>
+                                  <div class="border-button"><a href="about.html"> <i class="fa fa-user"></i> Mi perfil</a></div>
+                                </div>
+                                <div class="container">
+                                  <div class="row">
+                                    <div class="col-lg-12">
+                                      <div class="more-info">
+                                        <div class="row">
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-user"></i>
+                                            <h4><span>Population:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-globe"></i>
+                                            <h4><span>Territory:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-home"></i>
+                                            <h4><span>AVG Price:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <div class="main-button">
+                                              <a href="about.html">Explore More</a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div id="top-banner-3" class="banner">
+                              <div class="banner-inner-wrapper header-text">
+                                <div class="main-caption">
+                                  <h2>Take a Glimpse Into The Beautiful Country Of:</h2>
+                                  <h1>France</h1>
+                                  <div class="border-button"><a href="about.html"><i class="fa fa-user"></i> Mi perfil</a></div>
+                                </div>
+                                <div class="container">
+                                  <div class="row">
+                                    <div class="col-lg-12">
+                                      <div class="more-info">
+                                        <div class="row">
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-user"></i>
+                                            <h4><span>Population:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-globe"></i>
+                                            <h4><span>Territory:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-home"></i>
+                                            <h4><span>AVG Price:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <div class="main-button">
+                                              <a href="about.html">Explore More</a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div id="top-banner-4" class="banner">
+                              <div class="banner-inner-wrapper header-text">
+                                <div class="main-caption">
+                                  <h2>Take a Glimpse Into The Beautiful Country Of:</h2>
+                                  <h1>Thailand</h1>
+                                  <div class="border-button"><a href="about.html"><i class="fa fa-user"></i> Mi perfil</a></div>
+                                </div>
+                                <div class="container">
+                                  <div class="row">
+                                    <div class="col-lg-12">
+                                      <div class="more-info">
+                                        <div class="row">
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-user"></i>
+                                            <h4><span>Population:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-globe"></i>
+                                            <h4><span>Territory:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <i class="fa fa-home"></i>
+                                            <h4><span>AVG Price:</span></h4>
+                                          </div>
+                                          <div class="col-lg-3 col-sm-6 col-6">
+                                            <div class="main-button">
+                                              <a href="about.html">Explore More</a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <nav>
+                            <div class="controls">
+                              <label for="banner1"><span class="progressbar"><span class="progressbar-fill"></span></span><span class="text">1</span></label>
+                              <label for="banner2"><span class="progressbar"><span class="progressbar-fill"></span></span><span class="text">2</span></label>
+                              <label for="banner3"><span class="progressbar"><span class="progressbar-fill"></span></span><span class="text">3</span></label>
+                              <label for="banner4"><span class="progressbar"><span class="progressbar-fill"></span></span><span class="text">4</span></label>
+                            </div>
+                          </nav>
+                        </div>
+                      </section>
+
                     <!-- *****  Fin del banners  ***** -->
                 
 
