30c90a058818cd0f0bf64b7291b7a634ce2f86c2
Miguel Reyes <migueldeje171717@gmai.com>
2022-10-22 22:14:47 -0600

    feat: primeras rutas de acceso en la navegacion

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/oferta', function () {
+    return view('oferta');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
