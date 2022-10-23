dec66667c146a40d36d5e706c08f6172d51f402d
Miguel Reyes <migueldeje171717@gmai.com>
2022-10-22 20:36:02 -0600

    Feat: area de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,15 +13,19 @@
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Achivos adicionales CSS -->
-    
+    <!-- Archivo css Addicional  -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-woox-travel.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
 
 
   </head>
 
 <body>
 
-  <!-- ***** Inicio Precargador ***** -->
+  <!-- ***** inicio Preloader ***** -->
   <div id="js-preloader" class="js-preloader">
     <div class="preloader-inner">
       <span class="dot"></span>
@@ -32,31 +36,37 @@
       </div>
     </div>
   </div>
-  <!-- ***** precargador Fin ***** -->
+  <!-- ***** fin de Preloader ***** -->
 
-  <!-- ***** Inicio de Area del Encabezado ***** -->
+  <!-- ***** inicio de encabezado ***** -->
   <header class="header-area header-sticky">
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <nav class="main-nav">
-                    <!-- ***** Inicio del Logo ***** -->
+                    <!-- ***** inicio de logo ***** -->
                     <a href="index.html" class="logo">
-                        <img src="img\logo\Recurso 1.png" alt="">
+                        <img src="assets/images/logo.png" alt="">
                     </a>
-                    <!-- ***** Fin del Logo ***** -->
-                    <!-- ***** Inicio de Menu ***** -->
+                    <!-- ***** fin de logo ***** -->
+                    <!-- ***** Inicio de menu ***** -->
                     <ul class="nav">
-                        <li><a href="" class="active">Inicio</a></li>
-                        <li><a href="">Ofertas</a></li>
-                        <li><a href="">Busco Trabajo</a></li>
-                        <li><a href="">Contactar</a></li>
-                        <li><a href=""></a></li>
+                        <li><a href="index.html" class="active">Inicio</a></li>
+                        <li><a href="/oferta">Ofertas</a></li>
+                        <li><a href="/busco">Busco Trabajo</a></li>
+                        <li><a href="/contacto">Contactar</a></li>
+                        <li><a href="reservation.html"></a></li>
                     </ul>
                     <a class='menu-trigger'>
                         <span>Menu</span>
                     </a>
-                    <!-- ***** Fin de Menu ***** -->
+                    <!-- ***** fin de menu ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** fin de encabzado ***** -->
 
                     <!-- *****  Inicio del banners  ***** -->
 
@@ -224,22 +234,193 @@
                       </section>
 
                     <!-- *****  Fin del banners  ***** -->
-                
 
-  
+                    <!-- *****  Inicio  ***** -->
 
-  <footer>
-   
-  </footer>
+                    <div class="visit-country">
+                        <div class="container">
+                          <div class="row">
+                            <div class="col-lg-5">
+                              <div class="section-heading">
+                                <h2>OLPE</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-lg-8">
+                              <div class="items">
+                                <div class="row">
+                                  <div class="col-lg-12">
+                                    <div class="item">
+                                      <div class="row">
+                                        <div class="col-lg-4 col-sm-5">
+                                          <div class="image">
+                                            <img src="assets/images/country-01.jpg" alt="">
+                                          </div>
+                                        </div>
+                                        <div class="col-lg-8 col-sm-7">
+                                          <div class="right-content">
+                                            <h4>SWITZERLAND</h4>
+                                            <span>Europe</span>
+                                            <div class="main-button">
+                                              <a href="about.html">Explore More</a>
+                                            </div>
+                                            <p>Woox Travel is a professional Bootstrap 5 theme HTML CSS layout for your website. You can use this layout for your commercial work.</p>
+                                            <ul class="info">
+                                              <li><i class="fa fa-user"></i> 8.66 Mil People</li>
+                                              <li><i class="fa fa-globe"></i> 41.290 km2</li>
+                                              <li><i class="fa fa-home"></i> $1.100.200</li>
+                                            </ul>
+                                            <div class="text-button">
+                                              <a href="about.html">Need Directions ? <i class="fa fa-arrow-right"></i></a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div class="col-lg-12">
+                                    <div class="item">
+                                      <div class="row">
+                                        <div class="col-lg-4 col-sm-5">
+                                          <div class="image">
+                                            <img src="assets/images/country-02.jpg" alt="">
+                                          </div>
+                                        </div>
+                                        <div class="col-lg-8 col-sm-7">
+                                          <div class="right-content">
+                                            <h4>CARIBBEAN</h4>
+                                            <span>North America</span>
+                                            <div class="main-button">
+                                              <a href="about.html">Explore More</a>
+                                            </div>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                                            <ul class="info">
+                                              <li><i class="fa fa-user"></i> 44.48 Mil People</li>
+                                              <li><i class="fa fa-globe"></i> 275.400 km2</li>
+                                              <li><i class="fa fa-home"></i> $946.000</li>
+                                            </ul>
+                                            <div class="text-button">
+                                              <a href="about.html">Need Directions ? <i class="fa fa-arrow-right"></i></a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div class="col-lg-12">
+                                    <div class="item last-item">
+                                      <div class="row">
+                                        <div class="col-lg-4 col-sm-5">
+                                          <div class="image">
+                                            <img src="assets/images/country-03.jpg" alt="">
+                                          </div>
+                                        </div>
+                                        <div class="col-lg-8 col-sm-7">
+                                          <div class="right-content">
+                                            <h4>FRANCE</h4>
+                                            <span>Europe</span>
+                                            <div class="main-button">
+                                              <a href="about.html">Explore More</a>
+                                            </div>
+                                            <p>We hope this WoOx template is useful for you, please support us a <a href="https://paypal.me/templatemo" target="_blank">small amount of PayPal</a> to info [at] templatemo.com for our survival. We really appreciate your contribution.</p>
+                                            <ul class="info">
+                                              <li><i class="fa fa-user"></i> 67.41 Mil People</li>
+                                              <li><i class="fa fa-globe"></i> 551.500 km2</li>
+                                              <li><i class="fa fa-home"></i> $425.600</li>
+                                            </ul>
+                                            <div class="text-button">
+                                              <a href="about.html">Need Directions ? <i class="fa fa-arrow-right"></i></a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div class="col-lg-12">
+                                    <ul class="page-numbers">
+                                      <li><a href="#"><i class="fa fa-arrow-left"></i></a></li>
+                                      <li><a href="#">1</a></li>
+                                      <li class="active"><a href="#">2</a></li>
+                                      <li><a href="#">3</a></li>
+                                      <li><a href="#"><i class="fa fa-arrow-right"></i></a></li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="col-lg-4">
+                              <div class="side-bar-map">
+                                <div class="row">
+                                  <div class="col-lg-12">
+                                    <div id="map">
+                                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12469.776493332698!2d-80.14036379941481!3d25.907788681148624!2m3!1f357.26927939317244!2f20.870722720054623!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x88d9add4b4ac788f%3A0xe77469d09480fcdb!2sSunny%20Isles%20Beach!5e1!3m2!1sen!2sth!4v1642869952544!5m2!1sen!2sth" width="100%" height="550px" frameborder="0" style="border:0; border-radius: 23px; " allowfullscreen=""></iframe>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    
+                    <!-- *****  Fin   ***** -->
 
-
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  
-
-  <script>
-    
-  </script>
+                    <div class="call-to-action">
+                        <div class="container">
+                          <div class="row">
+                            <div class="col-lg-8">
+                              <h2>Are You Looking To Travel ?</h2>
+                              <h4>Make A Reservation By Clicking The Button</h4>
+                            </div>
+                            <div class="col-lg-4">
+                              <div class="border-button">
+                                <a href="reservation.html">Book Yours Now</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    
+                      <footer>
+                        <div class="container">
+                          <div class="row">
+                            <div class="col-lg-12">
+                              <p>Copyright © 2036 <a href="#">WoOx Travel</a> Company. All rights reserved.
+                              <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a> Distribution: <a href="https://themewagon.com target="_blank" >ThemeWagon</a></p>
+                            </div>
+                          </div>
+                        </div>
+                      </footer>
+                    
+                    
+                      <!-- Scripts -->
+                      <!-- Bootstrap core JavaScript -->
+                      <script src="vendor/jquery/jquery.min.js"></script>
+                      <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+                    
+                      <script src="assets/js/isotope.min.js"></script>
+                      <script src="assets/js/owl-carousel.js"></script>
+                      <script src="assets/js/wow.js"></script>
+                      <script src="assets/js/tabs.js"></script>
+                      <script src="assets/js/popup.js"></script>
+                      <script src="assets/js/custom.js"></script>
+                    
+                      <script>
+                        function bannerSwitcher() {
+                          next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
+                          if (next.length) next.prop('checked', true);
+                          else $('.sec-1-input').first().prop('checked', true);
+                        }
+                    
+                        var bannerTimer = setInterval(bannerSwitcher, 5000);
+                    
+                        $('nav .controls label').click(function() {
+                          clearInterval(bannerTimer);
+                          bannerTimer = setInterval(bannerSwitcher, 5000)
+                        });
+                      </script>
 
   </body>
 
